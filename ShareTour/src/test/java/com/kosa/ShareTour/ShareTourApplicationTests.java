9aedb01700cc046ac9456bdbf2f3bd60b6092a73
gomtaetae <rlaxodls108@hanmail.net>
2023-10-11 21:11:55 +0900

    Initial commit

---
+++ ShareTour/src/test/java/com/kosa/ShareTour/ShareTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosa.ShareTour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
